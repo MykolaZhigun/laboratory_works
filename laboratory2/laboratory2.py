ac43c8db196d93b495c609544477ded6a7239a55
Микола Жигун <125123753+MykolaZhigun@users.noreply.github.com>
2023-03-20 18:25:17 +0200

    Update laboratory2.py

--- laboratory2/laboratory2.py
+++ laboratory2/laboratory2.py
@@ -1,20 +1,19 @@
 def main():
+    exp_dictionary = {"1" : "Привsт ",
+                      "2" : "Світ",
+                      "3" : "!"}
+    var_massive = [1, 2, 4, 16, 2, 187]  # массив 
+    exp_cortage = (1, 2, 5, 16, 2, 187)  # кортеж
+
+
+    # Збираємо значення зі словника в одну строку
     exp_var = ""
-    exp_dictionary = {"1": "Привет ",
-                      "2": "Мир",
-                      "3": "!"}
-    var_massive = [1,2,4,16,2,187] #массив для зрівняння далі
-    exp_cortage = (1,2,5,16,2,187) #кортеж
-
-
-    #Выводим текст 'Привет Мир!' с помощью классов
     for key_values in exp_dictionary.values():
-        #Як працюють змінні
         exp_var += key_values
 
-    print(exp_var)
+    # Виводимо результати
+    print(exp_var) 
     print("_"*20)
-
     print(f"{var_massive}: - Массив")
     print(f"{exp_cortage}: - Кортеж")
 
