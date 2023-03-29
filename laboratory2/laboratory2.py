f3f41270a3254ea5012345ee9ac9819812dd5486
Микола Жигун <125123753+MykolaZhigun@users.noreply.github.com>
2023-03-29 10:08:28 +0300

    Update laboratory2.py

--- laboratory2/laboratory2.py
+++ laboratory2/laboratory2.py
@@ -1,23 +1,37 @@
 # -*- coding: utf-8 -*-
 
 def main():
-    exp_dictionary = {"1" : "Привіт ",
-                      "2" : "Світ",
-                      "3" : "!"}
-    var_massive = [1, 2, 4, 16, 2, 187]  # массив 
-    exp_cortage = (1, 2, 5, 16, 2, 187)  # кортеж
-
-
-    # Збираємо значення зі словника в одну строку
     exp_var = ""
+    exp_dictionary = {"1": "Привет ",
+                      "2": "Мир",
+                      "3": "!"}
+    var_massive = [] #массив для зрівняння далі
+    exp_cortage = (1,2,5,16,2,187) #кортеж
+
+
+    #Выводим текст 'Привет Мир!' с помощью классов
     for key_values in exp_dictionary.values():
+        #Як працюють змінні
         exp_var += key_values
 
-    # Виводимо результати
-    print(exp_var) 
+    print(exp_var)
     print("_"*20)
-    print(f"{var_massive}: - Массив")
+
+    for i in range(20):
+        var_massive.append(i)
+
+    print(f"{var_massive}: - Массив до змін")
+
+    var_massive.remove(1) # Видаляємо другий елемент в масиві
+    print(f"{var_massive}: - Массив після видалення елемента")
+
+    var_massive.append(40) # Додаємо число 40 до массиву
+    print(f"{var_massive}: - Массив після додавання елемента\n")
+
+    print("_"*20)
+
     print(f"{exp_cortage}: - Кортеж")
+    print(f"{exp_cortage[1]}: - Виводимо другий елемент в кортежі")
 
 if __name__ == '__main__':
     main()
